9d64d9ab23a2cbd781d514a53d57bf218d09e8c2
PancakeZoy <pancakezoy@gmail.com>
2024-08-04 05:14:51 -0600

    logo.png

--- scouter/__init__.py
+++ scouter/__init__.py
@@ -1,2 +1,3 @@
 from .Scouter import Scouter
 from .ScouterData import ScouterData
+__version__ = "0.1.0"
--- setup.py
+++ setup.py
@@ -2,23 +2,23 @@ from setuptools import setup, find_packages
 
 setup(
 	name='scouter',
-	version='0.1',
-	description='Gene Perturbation Prediction with LLM',
+	version='0.1.0',
+	description='A transcriptional response predictor for unseen genetic perturbtions with LLM embeddings',
 	packages=find_packages(include=['scouter', 'scouter.*']),
 	author='Ouyang Zhu, Jun Li',
 	author_email='ozhu@nd.edu',
 	url='https://github.com/PancakeZoy/scouter',
 	install_requires=[
 		'torch >= 2.0.0',
-		'tqdm',
-		'anndata',
-		'pandas',
-		'numpy',
-		'scanpy',
-		'seaborn',
-		'matplotlib',
-		'scikit-learn',
-		'scipy'
+		'tqdm >= 4.0.0',
+		'anndata >= 0.10.0',
+		'pandas >= 2.2.0',
+		'numpy >= 1.26.0',
+		'scanpy >= 1.10.0',
+		'seaborn >= 0.13.0',
+		'matplotlib >= 3.9.0',
+		'scikit-learn >= 1.5.0',
+		'scipy >= 1.14.0'
 	],
-	python_requires='>=3.6'
+	python_requires='>=3.11.0'
 )
